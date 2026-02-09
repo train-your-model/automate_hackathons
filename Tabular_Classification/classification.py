7df8982b20d061a21e83ed3ac5ed836498032670
train-your-model <crawlie777@gmail.com>
2026-02-10 03:16:20 +0530

    [Tabular_Classification - supplementary.py - REMOVED]
    [Tabular_Classification - classification.py - ADDED]
    1. Added the class dealing with the multi-record predictors.

--- Tabular_Classification/classification.py
+++ Tabular_Classification/classification.py
@@ -6,8 +6,6 @@ import os
 import pandas as pd
 import numpy as np
 
-from Tabular_Classification import supplementary as sp
-
 # F1
 def with_explore(func):
     def wrapper(self, *args, **kwargs):
@@ -36,7 +34,7 @@ def with_explore(func):
         print(self.object_list)
         print(self.float_list)
 
-        self.multi_record = sp.CheckMultiRecord(df=self.train_df,
+        self.multi_record = CheckMultiRecord(df=self.train_df,
                                                 pred_lst=self.object_list)
         self.multi_record.run()
 
@@ -308,3 +306,31 @@ class BasicPreprocess:
             self.threshold_miss_vars()
         if len(self.rej_miss_vars) != 0:
             self.df.drop(columns=self.rej_miss_vars, inplace=True)
+
+class CheckMultiRecord:
+
+    def __init__(self, df, pred_lst: list):
+        self.df = df
+        self.pred_lst = pred_lst
+        # Separator Based splits
+        self.split_w_comma = []
+        self.split_w_spaces = []
+        self.multi_record_pred = []
+
+    def multi_record_w_comma(self):
+        for col in self.pred_lst:
+            split_row_max = max(self.df[col].dropna().apply(lambda x: len(x.split(','))))
+            if split_row_max > 1:
+                self.split_w_comma.append(col)
+
+    @staticmethod
+    def multi_record(*lists, default=None):
+        non_empty = (lst for lst in lists if len(lst) > 0)
+        return next(non_empty, default)
+
+    def run(self):
+        self.multi_record_w_comma()
+
+        self.multi_record_pred = CheckMultiRecord(self.split_w_comma,
+                                                  self.split_w_spaces)
+        return self
